04fd556959fa778608b3442bc230dd45db2b9bf7
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-21 15:55:35 +1000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ const App = () => {
     'Hornsby Girls High School'
   ]
   return <>
-    <div className='grid place-content-center h-full bg-yellow-400 text-center select-none'>
-      <div className='flex flex-col gap-4'>
-        <header className='flex flex-col gap-4'>
-          <img className='w-24 h-24 bg-white rounded-2xl mx-auto shadow-xl' />
-          <h1 className='text-4xl'>Classy</h1>
+    <div className='grid place-content-center py-4 h-full bg-yellow-400 text-center select-none'>
+      <div className='flex flex-col gap-4 sm:gap-8 sm:rounded-2xl sm:shadow-2xl sm:p-8'>
+        <header className='flex flex-col gap-4 sm:gap-8'>
+          <img className='w-24 h-24 sm:w-36 sm:h-36 bg-white rounded-2xl mx-auto shadow-2xl' />
+          <h1 className='text-5xl sm:text-6xl'>Classy</h1>
         </header>
-        <div className='flex flex-col gap-2'>
-          Choose your school below to get started!
-          <ul className='bg-white rounded-2xl shadow-xl p-2 divide-y [&>li:first-child>div]:rounded-t-2xl [&>li:last-child>div]:rounded-b-2xl'>
+        <div className='flex flex-col gap-4 sm:gap-8 sm:text-xl'>
+          <span className='text-lg sm:text-2xl'>Choose your school below to get started!</span>
+          <ul className='bg-white rounded-2xl shadow-2xl p-4 divide-y [&>li:first-child>div]:rounded-t-2xl [&>li:last-child>div]:rounded-b-2xl'>
             {
               schools.map(
                 school => <li className='font-medium'>
-                  <div className='hover:bg-gray-100 transition p-4'>{school}</div>
+                  <div className='hover:bg-gray-200 transition p-4'>{school}</div>
                 </li>
               )
             }
