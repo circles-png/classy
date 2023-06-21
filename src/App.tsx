05faae608aeb2eef58f3e7755d8d565102b1a4eb
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-21 15:37:18 +1000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,19 @@ const App = () => {
   return <>
     <div className='grid place-content-center h-full bg-yellow-400 text-center select-none'>
       <div className='flex flex-col gap-4'>
-        <div className='flex flex-col gap-4'>
+        <header className='flex flex-col gap-4'>
           <img className='w-24 h-24 bg-white rounded-2xl mx-auto shadow-xl' />
           <h1 className='text-4xl'>Classy</h1>
-        </div>
-        <div className='flex flex-col gap-4'>
+        </header>
+        <div className='flex flex-col gap-2'>
           Choose your school below to get started!
-          <ul className='bg-white divide-y rounded-2xl'>
+          <ul className='bg-white rounded-2xl shadow-xl p-2 divide-y [&>li:first-child>div]:rounded-t-2xl [&>li:last-child>div]:rounded-b-2xl'>
             {
-              schools.map(school => <li className='p-4 font-medium transition'>{school}</li>)
+              schools.map(
+                school => <li className='font-medium'>
+                  <div className='hover:bg-gray-100 transition p-4'>{school}</div>
+                </li>
+              )
             }
           </ul>
         </div>
