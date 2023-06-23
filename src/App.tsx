3f824d7a4681409516e2eade35617f6a73cfc6eb
Matthew Li <matthew.li10@education.nsw.gov.au>
2023-06-23 23:58:34 +1000

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -30,19 +30,19 @@ const App = () => {
   return <>
     <div className='h-full text-center select-none overflow-clip'>
       <div className={classNames(
-        'grid place-content-center py-4 h-full bg-classy-300 text-white transition-transform ease-in-out duration-[3s] relative z-10',
+        'grid place-content-center py-4 h-full bg-classy-300 text-classy-50 transition-transform ease-in-out duration-[3s] relative z-10',
         currentSchool === ''
           ? 'translate-x-0'
           : 'translate-x-[100%]'
       )}>
         <div className='flex flex-col gap-4 sm:gap-8 sm:rounded-2xl sm:shadow-2xl sm:p-8'>
           <header className='flex flex-col gap-4 sm:gap-8'>
-            <img className='w-24 h-24 sm:w-36 sm:h-36 bg-white rounded-2xl mx-auto shadow-2xl' />
+            <img className='w-24 h-24 sm:w-36 sm:h-36 bg-classy-50 rounded-2xl mx-auto shadow-2xl' />
             <h1 className='text-5xl sm:text-6xl'>Classy</h1>
           </header>
           <div className='flex flex-col gap-4 sm:gap8 sm:text-xl'>
             <span className='text-lg sm:text-2xl'>Choose your school below to get started!</span>
-            <ul className='bg-classy-200 text-classy-800 rounded-2xl'>
+            <ul className='bg-classy-200 text-classy-900 rounded-2xl'>
               {
                 Object.entries(schools).map(
                   ([ , { hoverStyle, location, name } ]) => <li key={name}>
